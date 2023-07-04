6f51ea5eb5a7a22ad61a6b47badf52ca16ea6f2c
viktor-rumiievskyi <vityok05042002@gmail.com>
2023-07-04 20:00:10 +0300

    Admin authentication

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,3 +1,5 @@
+import clientPromise from '@/lib/mongodb'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
@@ -8,5 +10,6 @@ export default NextAuth({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-  ]
+  ],
+	adapter: MongoDBAdapter(clientPromise),
 })
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import { useSession, signIn, signOut } from "next-auth/react"
 export default function Home() {
 	const { data: session } = useSession();
 	if (!session) {
-		return 'not logged in'
-	}
-  return (
-    <div className="bg-blue-900 w-screen h-screen flex items-center">
+		return (
+			<div className="bg-blue-900 w-screen h-screen flex items-center">
 			<div className="text-center w-full">
-				<button className="bg-white p-2 px-4 rounded-lg" >Login winh Google</button>
+				<button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg" >Login winh Google</button>
 			</div>
 		</div>
+		);
+	}
+
+  return (
+		<div>Logged in {session.user.email}</div>
   )
 }
